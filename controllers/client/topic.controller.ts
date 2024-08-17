2ea28bb61519167a7fec00b9c8686d036e7fe474
vannghj <131614984+vannghj@users.noreply.github.com>
2024-08-17 19:19:14 +0700

    setup controller

---
+++ controllers/client/topic.controller.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from "express";
+import Topic from "../../models/topic.model";
+
+export const topics = async (req:Request, res: Response) => {
+    const topics = await Topic.find({
+        deleted: false,
+    });
+    console.log(topics);
+    res.render("client/pages/topics/index");
+}
--- routes/client/topic.route.ts
+++ routes/client/topic.route.ts
@@ -1,13 +1,8 @@
 import {Request, Response, Router} from "express";
-import Topic from "../../models/topic.model";
 const route:Router = Router();
 
-route.get("/", async (req:Request, res: Response) => {
-    const topics = await Topic.find({
-        deleted: false,
-    });
-    console.log(topics);
-    res.render("client/pages/topics/index");
-});
+import * as controller from "../../controllers/client/topic.controller";
+
+route.get("/", controller.topics);
 
 export const topicRoutes: Router = route;
