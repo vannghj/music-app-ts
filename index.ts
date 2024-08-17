f319a5191f1d928649c5e66915d14581277ea36b
vannghj <131614984+vannghj@users.noreply.github.com>
2024-08-17 18:59:06 +0700

    connect database

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import express, {Express, Request, Response} from "express";
+import dotenv from "dotenv";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
